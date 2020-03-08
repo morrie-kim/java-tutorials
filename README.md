d476a2ab7225a92255be8a1f75ae8788a6e429e1
morrie-kim <hacker7k@naver.com>
2020-03-08 23:26:02 +0900

    First Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Java Tutorials Project
+
+### Reference Documentation
+For further reference, please consider the following sections:
+
+
+### Additional Links
+These additional references should also help you:
+
---
+++ java-spring-data/src/main/java/com/morrie/tutorials/jsd/JavaSpringDataApplication.java
@@ -0,0 +1,18 @@
+package com.morrie.tutorials.jsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * Created by morrie kim on 2020/03/08.
+ */
+@EnableAspectJAutoProxy
+//@EnableFeignClients
+@SpringBootApplication
+public class JavaSpringDataApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringDataApplication.class, args);
+    }
+
+}
